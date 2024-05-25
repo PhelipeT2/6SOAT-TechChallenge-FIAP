03ccdab9793dabcc48b78a10e7802ab5bc8d6921
Phelipe Tozzi <phelipetozzi@hotmail.com>
2024-05-25 15:42:08 -0300

    enum categoria

---
+++ src/Domain/Enums/CategoriaEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum CategoriaEnum
+    {
+        [Description("Lanche")]
+        Lanche = 1,
+
+        [Description("Acompanhamento")]
+        Acompanhamento = 2,
+
+        [Description("Bebida")]
+        Bebida = 3,
+
+        [Description("Sobremesa")]
+        Sobremesa = 4
+    }
+}
